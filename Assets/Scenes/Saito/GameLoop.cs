eb6e52083026ee44116c2f4c6d6da2cdc486c73d
saitoSay <seinyacchi@gmail.com>
2022-05-05 12:00:58 +0900

    Event処理の完成

---
+++ Assets/Scenes/Saito/GameLoop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class GameLoop
+{
+    public static event Action OnGameStart;
+    public static event Action OnGameEnd;
+
+    public static void GameStart() => OnGameStart?.Invoke();
+    public static void GameEnd() => OnGameEnd?.Invoke();
+}

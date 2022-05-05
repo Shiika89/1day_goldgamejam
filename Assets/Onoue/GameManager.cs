655fa3548d9f1ded532082cacd934c72222fe748
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 14:12:41 +0900

    ゲームマネージャーを追加しました。

---
+++ Assets/Onoue/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get;private set; }
+    void Awake()
+    {
+        Instance = this;
+    }
+    public void GameStart()
+    {
+        //ボタンを押した時にゲームが始まる
+        GameLoop.GameStart();
+    }
+    public void GameEnd()
+    {
+        GameLoop.GameEnd();
+    }
+}

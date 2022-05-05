82dc688f16af70e389c949e930e2a48df2d442b8
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 11:49:59 +0900

    スコア完成

---
+++ Assets/Onoue/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score 
+{
+    static int s_totalScore;
+    public static int TotalScore { get => s_totalScore; private set { } }
+    static public void AddScore(int score)
+    {
+        s_totalScore += score;
+    }
+    static public void Init()
+    {
+        s_totalScore = 0;
+    }
+}

159b661271e8ed3013844ff75790b49b179d626b
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 15:21:27 +0900

    スコアを修正しました。

--- Assets/Onoue/Score.cs
+++ Assets/Onoue/Score.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class Score 
 {
     static int s_totalScore;
-    public static int TotalScore { get => s_totalScore; private set { } }
+    public static int TotalScore 
+    {
+        get => s_totalScore;
+        private set 
+        {
+            s_totalScore = value;
+            OnUpdateScore?.Invoke();
+        } 
+    }
+    public static event Action OnUpdateScore;
     static public void AddScore(int score)
     {
-        s_totalScore += score;
+        TotalScore += score;
     }
     static public void Init()
     {
-        s_totalScore = 0;
+        TotalScore = 0;
     }
 }

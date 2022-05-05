eef3f05c4419a1b346056c38c4552bb775d7d74f
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 13:32:12 +0900

    移動処理を追加しました。

---
+++ Assets/Onoue/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody _rb;
+    [SerializeField] Vector3 dir;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = dir.normalized * speed;
+    }
+}

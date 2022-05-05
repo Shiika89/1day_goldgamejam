7bb30c0e2e7aa638de07f965012583806ea752b1
sorude26 <sorude26@gmail.com>
2022-05-05 17:43:05 +0900

    追従機能スクリプトと振動機能スクリプトを追加した

---
+++ Assets/takeuchi/FollowObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField]
+    Transform _followTarget = default;
+    [SerializeField]
+    float _followSpeed = 1f;
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, _followTarget.position, _followSpeed * Time.deltaTime);
+    }
+}

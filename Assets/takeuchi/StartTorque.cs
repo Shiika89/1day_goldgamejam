220a01023c73e7a96f3366e2085be394b1af20b7
sorude26 <sorude26@gmail.com>
2022-05-05 20:37:37 +0900

    モデルを金にした

---
+++ Assets/takeuchi/StartTorque.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTorque : MonoBehaviour
+{
+    [SerializeField]
+    private float m_torque = 2f;
+    Rigidbody m_rb = default;
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.AddTorque(Vector3.up * Random.Range(-m_torque, m_torque),ForceMode.Impulse);
+    }
+}

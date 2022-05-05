a1c8994676d103835256fb988b36d855b64c7990
DESKTOP-IO50167\81804 <ukauka224@i.softbank.jp>
2022-05-05 13:47:54 +0900

    的の機能を持つScriptを追加

--- Assets/Nakajima/Scripts/TargetController.cs
+++ Assets/Nakajima/Scripts/TargetController.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(TargetMove))]
+[RequireComponent(typeof(BoxCollider))]
 public class TargetController : MonoBehaviour
 {
+    [Tooltip("当てた際に加算するスコア")]
+    [SerializeField]
+    int m_scoreValue = 100;
 
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if (collision.gameObject.tag == "Cannon") //大砲がヒットした場合
+        {
+            //スコア加算の処理
+            Score.AddScore(m_scoreValue);
+            //エフェクト生成
 
-    void Update()
-    {
-        
+            //自身を消去
+            Destroy(gameObject);
+        }
     }
 }

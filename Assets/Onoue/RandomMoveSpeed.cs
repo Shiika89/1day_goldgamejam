a5c5293d9ff5f74f2634148d65d1084c29791482
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 15:55:45 +0900

    スピードがランダムになる移動処理のスクリプト追加

---
+++ Assets/Onoue/RandomMoveSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMoveSpeed : MonoBehaviour
+{
+    Rigidbody _rb;
+    [SerializeField] Vector3 dir;
+    [SerializeField] float _minimumSpeed, _maxSpeed;
+
+    void Start()
+    {
+        float n = Random.Range(_minimumSpeed, _maxSpeed);
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = dir.normalized * n;
+    }
+}

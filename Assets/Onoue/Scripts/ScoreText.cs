b826cd35373678b733e0d95d461219d4a48f7d7c
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2022-05-05 16:20:30 +0900

    スコアテキストを追加

---
+++ Assets/Onoue/Scripts/ScoreText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreText : MonoBehaviour
+{
+    Text _text;
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+    public void SetScore(int score)
+    {
+        _text.text = score.ToString();
+    }
+}

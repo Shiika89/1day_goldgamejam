b587ed515461f56f1ed906792b71dae4c6643614
sorude26 <sorude26@gmail.com>
2022-05-05 12:21:57 +0900

    エフェクト操作スクリプトを追加した

---
+++ Assets/takeuchi/EffectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectType
+{
+    Explosion,
+    Shot,
+}
+/// <summary>
+/// エフェクトを再生する
+/// </summary>
+public class EffectManager : MonoBehaviour
+{
+    public static EffectManager Instance { get; private set; }
+    [SerializeField]
+    GameObject[] m_effectPrefabs;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void PlayEffect(EffectType type,Vector3 pos)
+    {
+        Instantiate(m_effectPrefabs[(int)type]).transform.position = pos;
+    }
+}
